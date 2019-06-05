7f473946b5913b3439a163fd8edceeedee993a36
Anxin <anxinyang114@gmail.com>
2019-06-05 11:16:39 -0700

    cc

--- main.js
+++ main.js
@@ -91,9 +91,10 @@
   !*** ./src/index.js ***!
   \**********************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'W:\\cc\\ccJS\\src\\index.js'");
+"use strict";
+
 
 /***/ })
 
